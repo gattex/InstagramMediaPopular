6485c2285a46490079e04875ab68f953ace1d1a4
gattex <damgattesco@gmail.com>
2015-06-24 16:19:20 -0300

    add instagram

---
+++ src/ar/com/instagram/media/popular/view/session/InstagramUser.java
@@ -0,0 +1,15 @@
+package ar.com.instagram.media.popular.view.session;
+
+/**
+ * Instagram user
+ * 
+ * @author Lorensius W. L. T
+ *
+ */
+public class InstagramUser {
+	public String id;
+	public String username;
+	public String fullName;
+	public String profilPicture;
+	public String accessToken;
+}
