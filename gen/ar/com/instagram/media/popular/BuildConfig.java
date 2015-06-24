7a04ac0ba23b046572f9d20c4e65951e9c081296
Damian Gattesco <damian@Damians-MacBook-Pro.local>
2015-06-23 23:01:39 -0300

    init commit

---
+++ gen/ar/com/instagram/media/popular/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ar.com.instagram.media.popular;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/ar/com/instagram/media/popular/MainActivity.java
@@ -0,0 +1,14 @@
+package ar.com.instagram.media.popular;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
